579fbb8e01857ad116c5998129732cc3e6d2059d
Sathira Silva <SathiraSofte@gmail.com>
2020-04-11 09:16:48 +0530

    Create KruskalMST.py

---
+++ Graph Theory/KruskalMST.py
@@ -0,0 +1,19 @@
+def find_set(u, parent):
+    while u != parent[parent[u]]:
+        parent[u] = parent[parent[u]]
+        u = parent[u]
+    return u
+
+def union(u, v, parent):
+    parent[find_set(v, parent)] = find_set(u, parent)
+
+def kruskals(n, edges):
+    parent = [i for i in range(n)]
+    edges.sort(key = lambda x:x[2])
+    s = 0
+    for u, v, w in edges:
+        if find_set(u, parent) != find_set(v, parent):
+            union(u, v, parent)
+            s += w
+    
+    return str(s)
